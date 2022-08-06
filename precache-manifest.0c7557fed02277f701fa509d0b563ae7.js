fd920cd140a0e987dfb0608724babafb4674515a
srinjay99 <66526373+srinjay99@users.noreply.github.com>
2022-08-06 20:52:03 +0530

    Updates

---
+++ precache-manifest.0c7557fed02277f701fa509d0b563ae7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b13c34c0cb0ca002d76a",
+    "url": "/TextUtils/static/css/main.105e83cb.chunk.css"
+  },
+  {
+    "revision": "b13c34c0cb0ca002d76a",
+    "url": "/TextUtils/static/js/main.b13c34c0.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "14506651abc0b1064f2d",
+    "url": "/TextUtils/static/js/runtime~main.14506651.js"
+  },
+  {
+    "revision": "fb34166b0a3d52dd0aec026ad0b0ac98",
+    "url": "/TextUtils/index.html"
+  }
+];
